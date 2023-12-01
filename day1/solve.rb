e08c50fa446e8dc5dddbcbb18f101a5288dffc82
Robin Opletal <me@robinopletal.com>
2023-12-01 13:16:51 +0100

    feat(day1): part two

--- day1/solve.rb
+++ day1/solve.rb
@@ -5,24 +5,39 @@ def numeric?(lookAhead)
 end
 
 arr = []
+word_digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 input.each_line do |line|
-  line_digit = ""
-  line.each_char do |char|
-    if numeric?(char)
-      # start with double digits based on the first numeric found
-      if line_digit.length == 0
-        line_digit << char
-      end
+  final_double_digit = ""
+  working_line = ""
 
-      # if line_digit is two chars already, cut last before appending new
-      if line_digit.length == 2
-        line_digit = line_digit[0...-1]
-      end
-      line_digit << char
+  # build line that we will actually iterate on, replacing all occurences of word_digits
+  # after each character
+  line.each_char do |char|
+    working_line << char
+    word_digits.each_with_index do |word_digit, idx|
+      working_line = working_line.gsub(word_digit.to_s, (idx + 1).to_s)
     end
   end
-  arr << line_digit
+
+  working_line.each_char do |char|
+    if numeric?(char)
+      # start with double digits based on the first numeric found
+      if final_double_digit.empty?
+        final_double_digit << char
+      end
+
+      # if final_double_digit is two chars already, cut last before appending new
+      if final_double_digit.length > 1
+        final_double_digit = final_double_digit[0...-1]
+      end
+
+      final_double_digit << char
+    end
+  end
+  arr << final_double_digit
 end
 
-puts arr.map(&:to_i).sum
+puts "result: #{arr.map(&:to_i).sum}"
+
+input.close
