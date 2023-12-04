c41ac42cda82bbefe360b14d1ee3216e0fc8fe6f
Robin Opletal <me@robinopletal.com>
2023-12-04 10:43:17 +0100

    refactor(day1): return both parts at once

--- day1/solve.rb
+++ day1/solve.rb
@@ -4,8 +4,6 @@ def numeric?(lookAhead)
   lookAhead.match?(/[[:digit:]]/)
 end
 
-arr = []
-
 mapping = {
   'one' => '1',
   'two' => '2',
@@ -18,23 +16,30 @@ mapping = {
   'nine' => '9',
 }
 
+parts = { 1 => [], 2 => [] }
+
 input.each_line do |line|
   ## swap reggied based on if you want part 1 or part 2 solution
   # reggied = line.scan(/(?=(\d))/).flatten
-  reggied = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
-  mapped = []
+  # reggied = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
+  reggies = [line.scan(/(?=(\d))/).flatten, line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten]
 
-  reggied.each do |reggie|
-    if mapping.include? reggie
-      mapped << mapping[reggie]
-    else
-      mapped << reggie
+  reggies.each_with_index do |r, idx|
+    mapped = []
+
+    r.each do |reggie|
+      if mapping.include? reggie
+        mapped << mapping[reggie]
+      else
+        mapped << reggie
+      end
     end
+    parts[idx + 1] << "#{mapped[0]}#{mapped[-1]}"
   end
-
-  arr << "#{mapped[0]}#{mapped[-1]}"
 end
 
 input.close
 
-puts arr.map(&:to_i).sum
+1.upto(2).each do |i|
+  puts parts[i].map(&:to_i).sum
+end
