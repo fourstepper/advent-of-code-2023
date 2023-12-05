5d743ae9b6fde59ee83c629e17d72c2dd6ce6b5e
Robin Opletal <me@robinopletal.com>
2023-12-05 23:24:55 +0100

    refactor(day5): part2
    done in 30 minutes compared to... literally unfinished, running for 8+
    hours

--- day5/solve.rb
+++ day5/solve.rb
@@ -31,30 +31,40 @@ data['seeds'].each do |seed|
   end
 end
 
+p part1
+
 part2 = nil
-seeds_from_ranges = []
+location = 0
+hit = 0
 
-data['seeds'].each_slice(2).to_a.each do |i|
-  [*i[0]..(i[0] + i[1])].each do |seed|
-    location = seed
-    data.each do |key, value|
-      if key == 'seeds'
-        next
-      end
+seed_ranges = data['seeds'].each_slice(2).to_a.map { |item| [item[0], item[0] + item[1]] }
+seed_ranges_min = seed_ranges.min[0]
+seed_ranges_max = seed_ranges.max[1]
 
-      value.each_slice(3).to_a.each do |i|
-        if location.between?(i[1], i[1] + i[2])
-          location += i[0] - i[1]
-          break
-        end
-      end
+until part2
+  hit = location
+
+  data.reverse_each do |key, value|
+    if key == 'seeds'
+      next
     end
 
-    if !part2 || location < part2
-      part2 = location
+    value.each_slice(3).each do |i|
+      if hit.between?(i[0], i[0] + i[2])
+        hit += i[1] - i[0]
+        break
+      end
     end
   end
+
+  if hit > seed_ranges_min || hit < seed_ranges_max
+    seed_ranges.each do |range|
+      if hit.between?(range[0], range[1])
+        part2 = location
+      end
+    end
+  end
+  location += 1
 end
 
-p part1
 p part2
