364f15763a0ff0318cfc61a2d0910dd3883df265
Robin Opletal <me@robinopletal.com>
2023-12-08 12:27:58 +0100

    feat(day8): part2

--- day8/solve.rb
+++ day8/solve.rb
@@ -15,6 +15,7 @@ data.map { |e| e.chomp }.drop(1).drop(1).each do |item|
   parsed[equal_split[0]] = equal_split[1].split(',').map(&:strip)
 end
 
+### part1 start
 steps = 0
 go_to = "AAA"
 
@@ -28,5 +29,32 @@ until go_to == "ZZZ"
     steps += 1
   end
 end
-
 p steps
+
+### part2 start
+a_end = []
+parsed.each do |item|
+  if item[0].end_with? 'A'
+    a_end << item[0]
+  end
+end
+
+total_steps = []
+
+a_end.each do |search_for|
+  go_to = search_for
+  steps = 0
+  until go_to.end_with? 'Z'
+    instructions.each_char do |char|
+      if char == 'L'
+        go_to = parsed[go_to][0]
+      else
+        go_to = parsed[go_to][1]
+      end
+      steps += 1
+    end
+  end
+  total_steps << steps
+end
+
+p total_steps.inject(&:lcm)
