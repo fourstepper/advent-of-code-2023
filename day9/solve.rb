0a63cd4168f9f1ea0adf4e0c27fdcfd36bd14661
Robin Opletal <me@robinopletal.com>
2023-12-10 14:49:38 +0100

    feat(day9): rewrite into recursive funcs, solve p2
    first solution wasn't terrible and would work fine "in the real world",
    but this is way cleaner...

--- day9/solve.rb
+++ day9/solve.rb
@@ -4,36 +4,36 @@ data = input.readlines.map(&:chomp).map { |e| e.split(' ') }.map { |e| e.map(&:t
 
 input.close()
 
-sequences = {}
-data.each_index do |idx|
-  sequences[idx] = []
-  sequences[idx] << data[idx]
-end
+def p1(arr)
+  if arr.all?(0)
+    return 0
+  end
 
-sequences.each_value do |v|
-  idx = 0
-  until v[-1].uniq.count == 1 && v[-1][0] == 0 do
-    to_append = []
-    v[idx].each_with_index do |d, i|
-      if i == 0
-        next
-      end
-
-      to_append << v[idx][i] - v[idx][i - 1]
+  to_append = []
+  arr.each_with_index do |d, i|
+    if i == 0
+      next
     end
 
-    v << to_append
-    idx += 1
+    to_append << arr[i] - arr[i - 1]
   end
+  return arr[-1] + p1(to_append)
 end
 
-p1 = []
-sequences.each_value do |s|
-  to_append = 0
-  s.each do |i|
-    to_append += i[-1]
+def p2(arr)
+  if arr.all?(0)
+    return 0
   end
-  p1 << to_append
+
+  to_append = []
+  arr.each_with_index do |d, i|
+    unless arr.length - 1 == i
+      to_append << arr[i + 1] - arr[i]
+    end
+  end
+
+  return arr[0] + p2(to_append)
 end
 
-p p1.sum
+p data.map { |e| p1(e) }.sum
+p data.map { |e| p2(e) }.sum
